d3f30459f589176847b7ce984bbd25797a7f90fe
Richard Brosius <rich@ip-172-31-26-117.us-east-2.compute.internal>
2019-02-24 19:08:20 +0000

    Add dynamic game cards to homepage

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,26 @@
 from flask import Flask, render_template, redirect, url_for
 
 
+
+# cms checks configuration file for all modules
+# which should be updated to give updated content
+# Content updated approximately daily
+# for the home page for now this is just the game 
+# from content_mangement import cms, cms_stream
+# content = cms.load()
+# tweets = cms_stream.load('tweets')
+
+# standin
+content = {'games':[['2-24-19','Buffalo', 'Pittsburgh'],
+                    ['2-24-19', 'Chicago', 'Philadelphia']]}
+
+
 # Define the WSGI application object
 app = Flask(__name__)
 
 # Configurations
 app.config.from_object('config')
 
-# Define a database connection
-# From db import Connection
-# db = Connection()
 
 @app.route("/")
 def gohome():
@@ -19,7 +30,8 @@ def gohome():
 # by modules and controllers
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    #return render_template('home.html')
+    return render_template('home.html', CONTENT=content)
 
 # Sample HTTP error handling
 @app.errorhandler(404)
